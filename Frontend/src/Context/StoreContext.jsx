25877920cc7cfddb6f9eec1d921c368d1169adef
Prakhar <playingprakhar122@gmail.com>
2026-02-19 22:19:11 +0530

    implemented add to cart and remove from cart functionality in storeContext

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -1,12 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 export const StoreContext = createContext(null);
 const StoreProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      if (!prev[itemId]) return prev;
+
+      const updatedCart = { ...prev, [itemId]: prev[itemId] - 1 };
+
+      if (updatedCart[itemId] <= 0) {
+        delete updatedCart[itemId];
+      }
+
+      return updatedCart;
+    });
+  };
+
   const contextValue = {
     food_list,
   };
   return (
-    <StoreContext.Provider value={contextValue}>
+    <StoreContext.Provider
+      value={{ contextValue, addToCart, removeFromCart, cartItems }}
+    >
       {children}
     </StoreContext.Provider>
   );
