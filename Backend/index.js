720ee64ff78e9b95faf7821a9a30cbcf39308442
Prakhar <playingprakhar122@gmail.com>
2026-03-09 21:00:59 +0530

    connect mongoDB with server successfully

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./Config/databaseConnect";
+
 dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+connectDB();
+
 const port = process.env.PORT || 8000;
-app.listen(
-  (port,
-  () => {
-    console.log(`Server Listening on this PORT: ${port}`);
-  }),
-);
+app.get("/", (req, res) => {
+  res.send("Hi I am Prakhar Joshi!");
+});
+
+app.listen(port, () => {
+  console.log(`Server Listening on this PORT: ${port}`);
+});
