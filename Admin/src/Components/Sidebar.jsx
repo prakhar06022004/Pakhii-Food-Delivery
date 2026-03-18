baed69b08b255f604d40ade62ad0eb3c2f8abb3e
Prakhar <playingprakhar122@gmail.com>
2026-03-18 21:13:32 +0530

    create responsive sidebar in admin panel

--- Admin/src/Components/Sidebar.jsx
+++ Admin/src/Components/Sidebar.jsx
@@ -1,7 +1,24 @@
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { CiBoxList } from "react-icons/ci";
+import { FiBox } from "react-icons/fi";
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="flex flex-col items-end gap-10 sm:w-80 w-20 py-5 border-r min-h-screen border-gray-500">
+      <div className="flex sm:w-60 gap-2 shadow-gray-300 border-0 shadow h-fit p-2">
+        <IoIosAddCircleOutline size={25} />
+        <p className="font-outfit sm:block hidden">Add Items</p>
+      </div>
+      <div className="flex sm:w-60 gap-2 shadow-gray-300 border-0 shadow h-fit p-2">
+        <CiBoxList size={25} />
+        <p className="font-outfit sm:block hidden">List Items</p>
+      </div>
+      <div className="flex sm:w-60 gap-2 shadow-gray-300 border-0 shadow h-fit p-2">
+        <FiBox size={25} />
+        <p className="font-outfit sm:block hidden">Orders</p>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
