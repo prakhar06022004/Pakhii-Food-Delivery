75d83e0ca5f79ddfde612f922fac0a6d250bb2e7
Prakhar <playingprakhar122@gmail.com>
2026-03-27 22:39:21 +0530

    create jwt token,its middleware

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from "../Models/userModel.js";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import generateToken from "../utils/jwtToken.js";
 
 const registerUser = async (req, res) => {
   try {
@@ -21,18 +22,23 @@ const registerUser = async (req, res) => {
           "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character",
       });
     }
-    const register = await userModel.findOne({ email });
-    if (register) {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
       return res.json({ success: false, message: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const newUser = await userModel.create({
       name,
       password: hashedPassword,
       email,
     });
+    const token = generateToken(newUser._id);
+
     console.log(newUser);
-    return res.status(201).json({ success: true, message: "User registered" });
+    return res
+      .status(201)
+      .json({ success: true, message: "User registered", token });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
@@ -56,15 +62,26 @@ const loginUser = async (req, res) => {
         .json({ success: false, message: "Password required" });
     }
 
-    const login = await userModel.findOne({ email });
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
 
-    const isMatch = await bcrypt.compare(password, login.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res
         .status(400)
         .json({ success: false, message: "Password did not match" });
     }
-    return res.status(200).json({ success: true, message: "Login successful" });
+    const token = generateToken(user._id);
+    return res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
