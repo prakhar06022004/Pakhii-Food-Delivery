32137c218c36820c53fa1043918c9552ca9ccee1
Prakhar <playingprakhar122@gmail.com>
2026-04-11 20:42:40 +0530

    .

--- Backend/Routes/cartRoute.js
+++ Backend/Routes/cartRoute.js
@@ -1,8 +1,15 @@
-import { accessCart,addToCart } from "../Controllers/cartController.js";
+import {
+  accessCart,
+  addToCart,
+  completeRemoveFromCart,
+  removeFromCart,
+} from "../Controllers/cartController.js";
 import authMiddleware from "../Middleware/jwt.js";
 import express from "express";
 
 const cartRouter = express.Router();
 cartRouter.get("/", authMiddleware, accessCart);
 cartRouter.post("/add", authMiddleware, addToCart);
+cartRouter.post("/removeCount", authMiddleware, removeFromCart);
+cartRouter.post("/completeRemove", authMiddleware, completeRemoveFromCart);
 export default cartRouter;
